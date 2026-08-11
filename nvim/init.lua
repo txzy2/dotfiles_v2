c950047ad26eec8fabcf10bd6b3cde737802f865
Kamaev Anton <alk@m2lab.ru>
2026-08-11 15:19:59 +0300

    Add new features

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,19 @@ vim.opt.termguicolors = true
 vim.opt.swapfile = false
 vim.opt.clipboard = 'unnamedplus'
 
+vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<cr>', { desc = 'Close buffer' })
+
+vim.keymap.set('i', '-', function()
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  local char = line:sub(col, col)
+  if char == '' or char == ' ' or char == '(' or char == '[' or char == '{' then
+    return '->'
+  else
+    return '-'
+  end
+end, { expr = true })
+
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -16,10 +16,37 @@ return {
         automatic_enable = true,
       })
 
+      local function go_to_definition()
+        vim.lsp.buf.definition({
+          reuse_win = true,
+          on_list = function(result)
+            if #result.items == 0 then
+              return
+            end
+            if #result.items == 1 then
+              local item = result.items[1]
+              vim.fn.cursor(item.lnum, item.col)
+              return
+            end
+            vim.fn.setloclist(0, result.items, 'r')
+            vim.cmd('lopen')
+            local augroup = vim.api.nvim_create_augroup('LspDefAutoClose', { clear = true })
+            vim.api.nvim_create_autocmd('WinLeave', {
+              group = augroup,
+              once = true,
+              callback = function()
+                vim.cmd('lclose')
+                pcall(vim.api.nvim_del_augroup_by_id, augroup)
+              end,
+            })
+          end,
+        })
+      end
+
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local opts = { buffer = args.buf }
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gd', go_to_definition, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
           vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
           vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
