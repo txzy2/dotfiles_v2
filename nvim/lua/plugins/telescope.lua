d4f29ec57c1158d53cc41adec7fd8b32805de090
Kamaev Anton <alk@m2lab.ru>
2026-08-11 15:30:54 +0300

    fix telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,7 +4,9 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local telescope = require('telescope.builtin')
-      vim.keymap.set('n', '<C-p>', telescope.find_files, { desc = 'find files' })
+      vim.keymap.set('n', '<C-p>', function()
+        telescope.find_files({ hidden = true, no_ignore = true })
+      end, { desc = 'find files' })
       vim.keymap.set('n', '<C-f>', telescope.live_grep, { desc = 'grep text' })
       vim.keymap.set('n', '<C-b>', telescope.buffers, { desc = 'buffers' })
       vim.keymap.set('n', '<C-g>', telescope.git_files, { desc = 'git files' })
